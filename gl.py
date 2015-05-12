7b36402d7a1ded10245704c6ac679fa05dd82477
Frank Song <spf.doudou@gmail.com>
2015-05-12 13:27:08 +0800

    csv2lua

---
+++ gl.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+keyLine = 1                     # csv file keys line
+keysArr = []
+typeLine = 2                    # csv file types line - string bool int
+typesArr = []
+defineLine = 4                  # >= 3
+
+inputDir = ""                  # *.csv
+inputEnds = ".csv"
+outputDir = ""                 # *.lua
+outputEnds = ".lua"
+
+name = ""
+level = 0
